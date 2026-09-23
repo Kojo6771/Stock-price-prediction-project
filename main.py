707a4511040f6c26e4ca24d1a9fb404dcb229046
Kojo6771 <kwadwo1092@googlemail.com>
2026-09-23 22:37:22 +0100

    Imported libraries and added dataset
    I have imported the necessary libraries, created a virtual environment, installed the packages, and added the dataset.

---
+++ main.py
@@ -0,0 +1,12 @@
+#Importing Libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+from tensorflow import keras
+import os
+from datetime import datetime
+
+import warnings
+warnings.filterwarnings("ignore")
